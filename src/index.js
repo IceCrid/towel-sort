4de425a93b9c7eab19ed6c6049f4bc3e82d2b5b0
IceCrid <icecrid@gmail.com>
2022-08-12 18:47:41 +0300

    feat: implement function towel sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,26 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let matrixSort = [];
+
+  // Check, if the parameters are not transmitted, return an empty array []
+  if (!matrix || !matrix.length) {
+    return [];
+  }
+
+  matrix.forEach((element, index) => {
+    let result = [];
+
+    if (index % 2 !== 0) {
+      result = [...element].reverse();
+    }
+
+    if (index % 2 == 0) {
+      result = [...element];
+    }
+
+    matrixSort.push(...result);
+  });
+
+  return matrixSort;
 }
